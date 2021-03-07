294b9aa27b1ffb525222afb5e8cb247139c815c8
urmatawil <susurrus.ater@gmail.com>
2021-03-07 01:44:26 -0300

    funcion generar mazo

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,33 @@
-console.log("Hola")
+// C = club, H = Heart, S = Spade, D = Diamond
+
+let deck = [];
+const tipos = ["C","H","S","D"];
+const especiales = ["A","J","Q","K"];
+
+const crearDeck = ()=> {
+    //generamos el arreglo de cartas por su numeracion y tipo
+    for (let i = 2; i <= 10; i++) {
+        for(const tipo of tipos) {
+            deck.push(i+tipo)
+        }           
+    }
+    //generamos el arreglo de cartas especiales y su tipo
+    for (const tipo of tipos) {
+
+        for (const esp of especiales) {
+            deck.push(esp + tipo)
+        }
+    }
+    //utilizamos _.shuffle() de la libreria underscore para mezclar los numeros
+    deck = _.shuffle(deck)
+    console.log(deck)
+    return deck;
+}
+
+crearDeck()
+
+/*-------Pedir Carta---------*/
+
+const pedirCarta = ()=>{
+    
+}
